b3993e94d2e91e693da9706a5a691d47d77277a7
Frizzybman11 <Frizzybman11@aol.com>
2024-04-09 14:44:00 -0400

    Basic game info

---
+++ script.js
@@ -0,0 +1,13 @@
+const gameboard = {
+    spaces: [1, 2, 3, 4, 5, 6, 7, 8, 9]
+}
+
+const playerOne = {
+    name: "blank",
+    marker: "X"
+}
+
+const playerTwo = {
+    name: "blank",
+    marker: "O"
+}
