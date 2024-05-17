18c10e9f32e0620b0b39110bcccd7c37393070cc
Frizzybman11 <Frizzybman11@aol.com>
2024-05-17 13:58:46 -0400

    While Loop code, currently not working

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ function game(playerOne, playerTwo, turnHeading){
     const gameboard = {
         spaces: [0, 1, 2, 3, 4, 5, 6, 7, 8]
     }
+
+    const gametable = document.getElementById("gameTable")
+
     const winConditions = {
         row1: [0, 1, 2],
         row2: [3, 4, 5],
@@ -12,13 +15,65 @@ function game(playerOne, playerTwo, turnHeading){
         diag1: [0, 4, 8],
         diag2: [2, 4, 6]
     }
+
     let gameStatus = "start"
-    let activeSquares = gameboard.spaces
+    let turnStatus = "one"
+
     while (gameStatus = "start"){
-        let turnStatus = "one"
         while (turnStatus = "one"){
             turnHeading.textContent = playerOne.name + "'s turn!"
-            
+            gametable.addEventListener('click', function (e){
+                const cell = e.target.closest('td');
+                console.log(cell.id)
+                let num = cell.id.slice(-1)
+                num = Number(num)
+                if (gameboard.spaces.indexOf(num) >= 0){
+                    playerOne.spaces.push(num)
+                    cell.textContent = playerOne.marker
+                    num = gameboard.spaces.indexOf(num)
+                    gameboard.spaces.splice(num, 1)
+                    playerOne.spaces.sort()
+                    console.log(playerOne.spaces)
+                    console.log(gameboard.spaces)
+                } else {
+                    console.log("Invalid space. Try again!")
+                }
+            })
+            turnStatus = "two"
+            gametable.removeEventListener('click', function (e){})
+            console.log("test")
+            return
+            //This If statement isn't used but deleting it breaks everything
+            if (userChoice in activeSquares){
+                playerOne.spaces.push(userChoice)
+                console.log(playerOne.spaces)
+            } else {
+                console.log("Invalid space. Try again!")
+            }
+            return
+        }
+        while (turnStatus = "two"){
+            turnHeading.textContent = playerTwo.name + "'s turn!"
+            gametable.addEventListener('click', function (e){
+                const cell = e.target.closest('td');
+                console.log(cell.id)
+                let num = cell.id.slice(-1)
+                num = Number(num)
+                if (gameboard.spaces.indexOf(num) >= 0){
+                    playerTwo.spaces.push(num)
+                    cell.textContent = playerTwo.marker
+                    num = gameboard.spaces.indexOf(num)
+                    gameboard.spaces.splice(num, 1)
+                    playerTwo.spaces.sort()
+                    console.log(playerTwo.spaces)
+                    console.log(gameboard.spaces)
+                    turnStatus = "one"
+                    return
+                } else {
+                    console.log("Invalid space. Try again!")
+                }
+        
+            })
             if (userChoice in activeSquares){
                 playerOne.spaces.push(userChoice)
                 console.log(playerOne.spaces)
@@ -26,6 +81,7 @@ function game(playerOne, playerTwo, turnHeading){
                 console.log("Invalid space. Try again!")
             }
         }
+        
     }
 }
 
@@ -43,6 +99,7 @@ function boardHTML(){
     const boardCell6 = document.createElement("td")
     const boardCell7 = document.createElement("td")
     const boardCell8 = document.createElement("td")
+    boardTable.setAttribute("id", "gameTable")
     boardRow1.setAttribute("id", "row1")
     boardRow2.setAttribute("id", "row2")
     boardRow3.setAttribute("id", "row3")
@@ -55,6 +112,15 @@ function boardHTML(){
     boardCell6.setAttribute("id", "cell6")
     boardCell7.setAttribute("id", "cell7")
     boardCell8.setAttribute("id", "cell8")
+    boardCell0.setAttribute("value", "0")
+    boardCell1.setAttribute("value", "1")
+    boardCell2.setAttribute("value", "2")
+    boardCell3.setAttribute("value", "3")
+    boardCell4.setAttribute("value", "4")
+    boardCell5.setAttribute("value", "5")
+    boardCell6.setAttribute("value", "6")
+    boardCell7.setAttribute("value", "7")
+    boardCell8.setAttribute("value", "8")
     boardRow1.append(boardCell0)
     boardRow1.append(boardCell1)
     boardRow1.append(boardCell2)
